c8485e4c5f3c5caa54e1318ccfa0978a56dcebae
lucas-m-d <68205310+lucas-m-d@users.noreply.github.com>
2022-03-24 23:41:57 +0000

    Add files via upload

---
+++ DFT.py
@@ -0,0 +1,56 @@
+# Lucas Dick's Maths IA
+# Requires Python 3.8.5 or greater, numpy, scipy and matplotlib to be installed
+import matplotlib.pyplot as plt
+import csv
+import scipy.linalg as linalg
+import numpy as np
+
+np.set_printoptions(formatter={'float_kind': '{:f}'.format})
+
+# import pandas
+SAMPLE_RATE = 44100
+with open("real-data-24-1.csv", newline='') as data:
+    starting_data = list(csv.reader(data))
+    # del starting_data[0]
+    starting_data = [float(i[0]) for i in starting_data]
+    sample_num = len(starting_data)
+
+T = sample_num/SAMPLE_RATE
+
+dft_matrix = linalg.dft(sample_num)
+complex_data = starting_data @ dft_matrix
+# complex_data = complex_data[:sample_num//2
+complex_data = [i for i in complex_data]
+
+data_arguments = [np.angle(i) for i in complex_data]
+data_absolute = [2*np.abs(complex_data[i]) / sample_num
+                 for i in range(sample_num)]
+
+plt.plot(np.arange(0, SAMPLE_RATE//2, SAMPLE_RATE/sample_num),
+         data_absolute[:sample_num//2])
+plt.grid(color='gray', linestyle='-')
+
+notable_waves = [[SAMPLE_RATE*i/sample_num, data_absolute[i], data_arguments[i]]
+                 for i in range(sample_num) if data_absolute[i] > 0.003 and i < SAMPLE_RATE//2]
+
+with open("out.csv", 'w', newline='') as out:
+    writer = csv.writer(out)
+    writer.writerow([i[0] for i in notable_waves])
+    writer.writerow([i[1] for i in notable_waves])
+    writer.writerow([i[2] for i in notable_waves])
+print([i[0] for i in notable_waves])
+print([i[1] for i in notable_waves])
+print([i[2] for i in notable_waves])
+
+# display waves
+'''
+values = np.arange(0, T, SAMPLE_RATE)
+fourier_waves = [np.cos(2*np.pi*i*values + data_arguments[i])
+                 for i in range(sample_num//2)]
+
+plt.plot(values, fourier_waves[1] + fourier_waves[2])
+plt.grid(True)
+plt.axhline(y=0, color='k')
+plt.show()
+'''
+plt.show()
---
+++ synthesizer.py
@@ -0,0 +1,39 @@
+# requires python 3.8.5 or greater
+# requires numpy and scipy to be installed
+
+import numpy as np
+import scipy.io.wavfile
+import csv
+
+sample_freq = 44100
+length = 3
+datapoints = np.arange(0, length, 1/sample_freq)
+fundamental = 440
+with open('out.csv', newline='') as out:
+    rows = []
+    reader = csv.reader(out)
+    for row in reader:
+        rows.append(row)
+    Fk = [float(i) for i in rows[0]]
+    Ak = [float(i) for i in rows[1]]
+    Pk = [float(i) for i in rows[2]]
+
+fundamental_ratio = fundamental/Fk[0]
+Fk = np.multiply(Fk, fundamental_ratio)
+coefficient_len = len(Fk)
+waves = []
+for i in range(coefficient_len):
+    cos_input = np.multiply(datapoints, 2*np.pi*Fk[i])
+    cos_input += Pk[i]
+    cos = np.cos(cos_input)
+    cos = np.multiply(cos, Ak[i])
+    waves.append(cos)
+
+output = waves[0].copy()
+
+for i in range(1, coefficient_len):
+    output = np.add(output, waves[i])
+
+output = np.multiply(output, 1)
+
+scipy.io.wavfile.write("clarinet_synth_1.NIST", 44100, output)
